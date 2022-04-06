26eed61cb96d397639cbd3e8410fa03cd49a38f7
jonas1569 <j.schneider@tu-ilmenau.de>
2022-04-06 17:06:47 +0200

    fixing the merge operation for duckdb mode

--- example.py
+++ example.py
@@ -6,57 +6,32 @@ import sqlite3
 
 con = sqlite3.connect("grizzly.db")
 grizzly.use(RelationalExecutor(con, SQLGenerator("sqlite")))
-df1 = grizzly.read_table("events")
+df = grizzly.read_table("events")
 
-df1 = df1[['theyear', 'numarticles']]
-df1 = df1[df1.theyear == 2014]
-df1.show(pretty=True)
+df1 = df[['globaleventid', 'actor1name', 'numarticles']]
+df2 = df[['globaleventid', 'actor2name', 'numarticles']]
 
-df = df1.diff(mode='pandas')
+pdf1 = df1.sql_to_dataframe()
+pdf2 = df2.sql_to_dataframe()
+
+import duckdb
+con = duckdb.connect()
+rel1 = con.from_df(pdf1)
+rel2 = con.from_df(pdf2)
+res = con.execute("SELECT * FROM pdf1").fetchdf()
+
+
+print(df1.merge(mode='duckdb', objs=df2))
+
+
+
+
+'''pdf = df1.merge(mode='duckdb', objs=df2)
+#print(type(pdf), '\n', pdf)
+
+print(df1.concat_tables(mode='pyarrow', tables=df2))
+df = df1.assign(test_numart=lambda x: x.numarticles * 20)
 print(df)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''grobe Umsetzung von der Client-Side Execution Engine mit DuckDB
-print('Umsetzung der Client-Side Execution Engine mit DuckDB:')
-import duckdb
-
-#create an in-memory temporary database
-conn = duckdb.connect()
-
-#create a relation for every dataframe of the operation
-rel = conn.from_df(df.sql_to_dataframe(con1))
-rel1 = conn.from_df(df1.sql_to_dataframe(con1))
-rel2 = conn.from_df(df2.sql_to_dataframe(con2))
-
-#exceute the operation
-res = rel.filter('theyear = 2014').aggregate("count(theyear)")
-
-print(type(res), res)
-'''
-
-
-
-'''Umsetzungsmöglichkeit von ApacheArrow
-print('Umsetzung der Client-Side Execution Engine mit ApacheArrow:')
-import pyarrow as pa
-
-table1 = pa.Table.from_pandas(df.sql_to_dataframe(con1))
-
-res = table1.group_by("theyear").aggregate([
-    ("theyear", "count", pa.compute.CountOptions(mode="all"))
-     ])
-
-print(type(res), res)
-'''
+pdf2 = pdf.merge(right=df)
+print(pdf2)'''
