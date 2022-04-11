bb94d6cbaa069ff3deae9e3f55044f5e2e132327
jonas1569 <j.schneider@tu-ilmenau.de>
2022-04-11 07:16:06 +0200

    changing client side execution approach + reimplement of the sql_to_dataframe()

--- example.py
+++ example.py
@@ -8,30 +8,32 @@ con = sqlite3.connect("grizzly.db")
 grizzly.use(RelationalExecutor(con, SQLGenerator("sqlite")))
 df = grizzly.read_table("events")
 
-df1 = df[['globaleventid', 'actor1name', 'numarticles']]
-df2 = df[['globaleventid', 'actor2name', 'numarticles']]
-
-pdf1 = df1.sql_to_dataframe()
-pdf2 = df2.sql_to_dataframe()
-
-import duckdb
-con = duckdb.connect()
-rel1 = con.from_df(pdf1)
-rel2 = con.from_df(pdf2)
-res = con.execute("SELECT * FROM pdf1").fetchdf()
-
-
-print(df1.merge(mode='duckdb', objs=df2))
-
-
+'''Beispiel der merge Funktion mit Hilfe auf Basis von Pandas'''
+print('Beispiel der merge Funktion mit Hilfe auf Basis von Pandas')
+df1 = df[["globaleventid", "actor1name", "numarticles"]]
+df2 = df[["globaleventid", "actor1name", "actor2name", "fractiondate"]]
+merged = df2.merge(other=df1,mode='pandas', left_on='actor1name', right_on='actor1name')
+print(type(merged), merged)
+print(merged.columns)
+print('_____________________________________________________')
+df3 = df[df["globaleventid"] == 470747760] # filter
+df3 = df3[["globaleventid", "actor1countrycode", "actor2countrycode"]]
+print(type(df3))
+df3.show(pretty=True)
+print('_____________________________________________________')
+print(df3.merge(other=(df2.merge(other=df1, mode='pandas', left_on='actor1name', right_on='actor1name')),mode='pandas', left_on='globaleventid', right_on='globaleventid_x', how='left'))
 
 
-'''pdf = df1.merge(mode='duckdb', objs=df2)
-#print(type(pdf), '\n', pdf)
-
-print(df1.concat_tables(mode='pyarrow', tables=df2))
-df = df1.assign(test_numart=lambda x: x.numarticles * 20)
-print(df)
-
-pdf2 = pdf.merge(right=df)
-print(pdf2)'''
+'''Beispiel der merge Funktion auf Basis von DuckDB'''
+print('Beispiel der merge Funktion mit Hilfe auf Basis von DuckDB')
+df1 = df[["globaleventid", "actor1name", "numarticles"]]
+df2 = df[["globaleventid", "actor1name", "actor2name", "fractiondate"]]
+merged = df2.merge(other=df1,mode='duckdb', left_on='actor1name', right_on='actor1name')
+print(type(merged), merged)
+print('_____________________________________________________')
+df3 = df[df["globaleventid"] == 470747760] # filter
+df3 = df3[["globaleventid", "actor1countrycode", "actor2countrycode"]]
+print(type(df3))
+df3.show(pretty=True)
+print('_____________________________________________________')
+print(df3.merge(other=(df2.merge(other=df1, mode='duckdb', left_on='actor1name', right_on='actor1name')), mode='duckdb', on='globaleventid', how='left'))
